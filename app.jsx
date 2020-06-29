0fb08ddd47e6607a64301aee3d459f1792af378f
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-06-29 18:10:32 +0800

    Component setTimeout

--- app.jsx
+++ app.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//建立一個 DOM 物件
-const displayTime = () => {
-    let nowTime = (
-        <div>
-            <span>nowTime:{new Date().toLocaleTimeString()}</span>
-        </div>
-    )
-    ReactDOM.render(nowTime,document.getElementById('root'));
+class ShowTime extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {time: new Date().toLocaleTimeString()}
+    }
+
+    componentDidMount(){
+        const upTime = () => {
+            this.setState({time: new Date().toLocaleTimeString()})
+        }
+        setInterval(upTime,2000)
+    }
+
+    componentDidUpdate(){
+        console.log(`大概兩秒的時候一直更新${this.state.time}`);
+    }
+
+    componentWillUnmount(){
+        console.log(`移除套件的時候大概是:${this.state.time}`);
+    }
+
+    render(){
+        return <h1>現在時間是：{this.state.time}</h1>
+    }
 }
 
-setInterval(displayTime,1000)
+ReactDOM.render(<ShowTime />,document.getElementById('root'))
 
-//使用 ReactDOM.render 把剛剛建立的 element 插入目標 DOM 中
+const removeComponent = () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+}
+
+setTimeout(removeComponent,5000);
