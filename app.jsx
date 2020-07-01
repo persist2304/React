0a06896cd0093d2e99aa8ef8d3ce332d559531ad
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-01 18:01:11 +0800

    簡單表格去取得值 使用： 1) form 2) value = this.state.value 3) submit

--- app.jsx
+++ app.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class TodoList extends React.Component{
+class EasyForm extends React.Component{
+
     constructor(props){
         super(props)
+        this.state = ({name: ``})
+        this._changeState = this._changeState.bind(this)
+        this._submitForm = this._submitForm.bind(this)
     }
-    render(){
-        let lists = this.props.objLists.map(list =>{
-            return <li key={list.id}>{list.list}</li>
-        })
 
-        return (
-            <ul>
-                {lists}
-            </ul>
+    _changeState(){
+        console.log(`輸入:${event.target.value}`)
+        this.setState({name: event.target.value})
+    }
+
+    _submitForm(){
+        console.log(`現在輸入是${this.state.name}`)
+        event.preventDefault()
+    }
+
+    render(){
+        const easyForm = (
+                <form onSubmit={this._submitForm}>
+                    <label>Name:</label>
+                    <input type="text"
+                           id="name"
+                           name="name"
+                           onChange={this._changeState}
+                           value={this.state.name}
+                    />
+                    <br/>
+                    <input type="submit"
+                           value="submit"
+                    />
+                </form>
+        )
+        return(
+            <div>
+                {easyForm}
+            </div>
         )
     }
 }
-
-let objLists = [
-    {id:'a',list:'打文章'},
-    {id:'b',list:'寫程式'},
-    {id:'c',list:'耍廢'}
-]
-
-ReactDOM.render(<TodoList objLists={objLists}/>,document.getElementById('root'));
-
+ReactDOM.render(<EasyForm/>,document.getElementById('root'));
