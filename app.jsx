ddf83d967321bd854c5cd7206dab40831c8348a7
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-01 11:37:32 +0800

    按鈕在 console 累加功能。 使用: 1) class 2) bind function 3) state 更動

--- app.jsx
+++ app.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class CheckButton extends React.Component{
+class AddButton extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = ({clickCount: 0})
+        this.addCount = this.addCount.bind(this)
+    }
 
-    writeButton(){
-        console.log('click it');
+    addCount(count){
+        console.log(`每次增加:${count}`)
+        this.setState({clickCount: this.state.clickCount+count},()=>{console.log(`點惹:${this.state.clickCount}下`)})
+        console.log(`點惹:${this.state.clickCount}下`)
+
+    }
+
+    componentDidUpdate(){
+        console.log(`Update點惹:${this.state.clickCount}下`)
     }
 
     render(){
-        return <input type="button" onClick={this.writeButton} value="點我看 console"/>
+        return <input type="button"
+                      onClick={this.addCount.bind(this,1)}
+                      value="click me"/>
     }
 }
-ReactDOM.render(<CheckButton/>,document.getElementById('root'))
+ReactDOM.render(<AddButton/>,document.getElementById('root'));
 
 
 
