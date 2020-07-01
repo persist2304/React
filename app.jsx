3ca611dbd02a78a8fa4dc3f04d4f14122d335304
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-01 11:35:10 +0800

    base : React init without class

--- app.jsx
+++ app.jsx
@@ -1,36 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class ShowTime extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {time: new Date().toLocaleTimeString()}
-    }
+class CheckButton extends React.Component{
 
-    componentDidMount(){
-        const upTime = () => {
-            this.setState({time: new Date().toLocaleTimeString()})
-        }
-        setInterval(upTime,2000)
-    }
-
-    componentDidUpdate(){
-        console.log(`大概兩秒的時候一直更新${this.state.time}`);
-    }
-
-    componentWillUnmount(){
-        console.log(`移除套件的時候大概是:${this.state.time}`);
+    writeButton(){
+        console.log('click it');
     }
 
     render(){
-        return <h1>現在時間是：{this.state.time}</h1>
+        return <input type="button" onClick={this.writeButton} value="點我看 console"/>
     }
 }
+ReactDOM.render(<CheckButton/>,document.getElementById('root'))
 
-ReactDOM.render(<ShowTime />,document.getElementById('root'))
 
-const removeComponent = () => {
-    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-}
 
-setTimeout(removeComponent,5000);
