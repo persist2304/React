8b2f68d7a0b38b058d954c2b980ca7f98250f7a7
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-01 14:51:59 +0800

    跟動資料時，會隨著資料顯示性別

--- app.jsx
+++ app.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class HelloTitle extends React.Component{
+
+    componentDidUpdate(){
+        console.log(`這次是${this.props.title}`)
+    }
+
+    render(){
+
+        let titleName = this.props.title == '先生'?
+            <h1>{this.props.title}!你好</h1>:
+            null
+
+
+        return <div>{titleName}</div>
+    }
+}
+
 class InputGender extends React.Component{
     constructor(props){
         super(props)
-        this.state = ({gender: ''})
+        this.state = ({gender: 'M'})
         this.changeGender = this.changeGender.bind(this)
     }
 
-    changeGender(strA){
+    changeGender(){
         console.log(event.target)
 
         this.setState({gender: event.target.value})
@@ -19,11 +36,21 @@ class InputGender extends React.Component{
     }
 
     render(){
+
+        let title;
+        this.state.gender == 'M'?
+            title = <HelloTitle title="先生"/>:
+            title = <HelloTitle title="小姐"/>
+
         return (
-        <select onChange={this.changeGender.bind(this)}>
-            <option value="M">male</option>
-            <option value="W">Female</option>
-        </select>
+            <div>
+                {title}
+                <select onChange={this.changeGender.bind(this)}>
+                    <option value="M">male</option>
+                    <option value="W">Female</option>
+                </select>
+            </div>
+
         )
     }
 }
