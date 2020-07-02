bf84701410d994837b9c8966715036209395362d
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-02 11:21:05 +0800

    簡單表格，取得表格資料，使用統一變數 1) setState({[changeName]})

--- app.jsx
+++ app.jsx
@@ -5,32 +5,75 @@ class EasyForm extends React.Component{
 
     constructor(props){
         super(props)
-        this.state = ({name: ``})
+        this.state = {
+            name: ``,
+            gender: `M`,
+            introduction:``,
+
+        }
         this._changeState = this._changeState.bind(this)
         this._submitForm = this._submitForm.bind(this)
     }
 
     _changeState(){
-        console.log(`輸入:${event.target.value}`)
-        this.setState({name: event.target.value})
+        let changeName = event.target.name
+        this.setState({[changeName]: event.target.value})
     }
 
     _submitForm(){
-        console.log(`現在輸入是${this.state.name}`)
+        console.log(`現在name是${this.state.name}`)
+        console.log(`現在gender是${this.state.gender}`)
+        console.log(`現在introduction是${this.state.introduction}`)
         event.preventDefault()
     }
 
     render(){
+        const name = (
+            <div>
+                <label>Name:</label>
+                <input type="text"
+                       id="name"
+                       name="name"
+                       onChange={this._changeState}
+                       value={this.state.name}
+                />
+            </div>
+        )
+
+        const gender = (
+            <div>
+                <label>gender:</label>
+                <select name="gender"
+                        id="gender"
+                        onChange={this._changeState}
+                        value={this.state.gender}
+                >
+                        <option value="M">male</option>
+                        <option value="W">female</option>
+                </select>
+            </div>
+        )
+
+        const textarea = (
+            <div>
+                <label>introduction:</label>
+                <br/>
+                <textarea name="introduction"
+                          id="introduction"
+                          cols="30"
+                          rows="10"
+                          onChange={this._changeState}
+                          value={this.state.introduction}
+                >
+                </textarea>
+            </div>
+        )
+
         const easyForm = (
                 <form onSubmit={this._submitForm}>
-                    <label>Name:</label>
-                    <input type="text"
-                           id="name"
-                           name="name"
-                           onChange={this._changeState}
-                           value={this.state.name}
-                    />
-                    <br/>
+                    {name}
+                    {gender}
+                    {textarea}
                     <input type="submit"
                            value="submit"
                     />
