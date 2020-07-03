7d0cbf225a685fd8051b6ab17ace7621b31e10c8
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-03 16:10:44 +0800

    攝氏華氏轉換 使用: 1)  同樣的輸入匡給不同的值 2) 使用組件值匡給子組件值 3) 子組件只負責輸出畫面，邏輯都在父組件值寫

--- app.jsx
+++ app.jsx
@@ -1,36 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class EasyForm extends React.Component{
-
-    constructor(props){
-        super(props)
-        this._submitForm = this._submitForm.bind(this)
-        this._filebox = React.createRef()
-    }
-    _submitForm(){
-        console.log(`選擇檔案為:${this._filebox.current.files[0].name}`)
-        event.preventDefault()
-    }
-
+//判斷溫度是否達沸點
+class Title extends React.Component {
     render(){
-        const uploadFileTitle = (
-            <h1>上傳檔案：</h1>
-        )
-        const submit = (
-            <input type="submit" value="submitForm"/>
-        )
-        const uploadFile = (
-            <input type="file" ref={this._filebox}/>
-        )
+        //溫度100度以上就到達沸點
+        return <h1>{(this.props.temperature>=100 ? '達到沸點!!!':'未到沸點...')}</h1>
+    }
+}
+
+class InputTemperature extends React.Component{
+    render(){
         return (
-            <form onSubmit={this._submitForm}>
-                {uploadFileTitle}
-                {uploadFile}
-                <br/>
-                {submit}
-            </form>
+            <div>
+                <span>目前輸入溫度是：{this.props.temperature}度{this.props.type}</span>
+                <input type="text"
+                       name="temperature"
+                       value={this.props.temperature}
+                       onChange={this.props.changeState}
+                />度{this.props.type}
+            </div>
         )
     }
 }
-ReactDOM.render(<EasyForm/>,document.getElementById('root'));
+
+class EasyForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = ({
+            temperature : 0,
+            type: ``,
+        })
+        this.changeState = this.changeState.bind(this)
+    }
+
+    toConvert(temperature,type){
+        return type == 'C'?
+            (temperature - 32) * 5 / 9:
+            (temperature * 9 / 5) + 32
+    }
+
+    changeState(type){
+        let temperature = window.event.target.value
+        this.setState({
+            [event.target.name]: temperature,
+            type: type
+        })
+    }
+
+    render() {
+        let temperature_C = this.state.type == "F"?
+            this.toConvert(this.state.temperature,'C'):
+            this.state.temperature
+        let temperature_F = this.state.type == "C"?
+            this.toConvert(this.state.temperature,'F'):
+            this.state.temperature
+        return(
+            <div>
+                <Title temperature={temperature_C} />
+                <InputTemperature type="C"
+                                  temperature={temperature_C}
+                                  changeState={this.changeState.bind(this,'C')}
+                />
+                <InputTemperature type="F"
+                                  temperature={temperature_F}
+                                  changeState={this.changeState.bind(this,'F')}
+                />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<EasyForm/>, document.getElementById('root'))
+
