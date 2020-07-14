5233e0c8203a380d192e7177d8cfdd95c92eb525
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-14 17:01:39 +0800

    Switch / Redirect 用法

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,37 +1,47 @@
-import React from 'react'
-import {Route, Link} from 'react-router-dom'
+import React from "react"
+import { Route, Link, Switch, Redirect } from "react-router-dom"
 
-class About extends React.Component{
-    render(){
-        console.log(this.props.match)
+class About extends React.Component {
+    render() {
         return (
             <div>
                 <h2>關於我們選單</h2>
                 <ul>
-                    <li>
-                        <Link to={`${this.props.match.url}/introd`}>理念介紹</Link>
-                    </li>
-                    <li>
-                        <Link to={`${this.props.match.url}/his`}>歷史改革</Link>
-                    </li>
+                    <li><Link to={`${this.props.match.url}`}>理念介紹</Link></li>
+                    <li><Link to={`${this.props.match.url}/his`}>歷史沿革</Link></li>
+                    <li><Link to={`${this.props.match.url}/story`}>品牌故事</Link></li>
                 </ul>
-                <Route exact path={`${this.props.match.path}/:type`} component={AboutContent}/>
+                <Switch>
+                    <Route exact path={`${this.props.match.path}`} component={Introd} />
+                    <Route path={`${this.props.match.path}/his`} component={His} />
+                    <Redirect from={`${this.props.match.path}/story`}
+                              to={{pathname:`${this.props.match.url}/his`,
+                                   search: "?hey=UCCU",
+                                   state:{name:'Referrer'}}} />
+                </Switch>
             </div>
         )
     }
 }
 
-class AboutContent extends React.Component{
-    render(){
-        console.log(this.props.match)
-        let content
-        this.props.match.params.type == 'introd'?
-            content = '這裡是理念介紹':
-            content = '這裡是歷史改革'
-        return (
-            <p>{content}</p>
-        )
+class Introd extends React.Component {
+    render() {
+        return <p>這裡是理念介紹</p>
     }
 }
 
-export {About}
+class His extends React.Component {
+    render() {
+        console.log(this.props.location.state)
+        return <p>這裡是歷史沿革</p>
+    }
+}
+
+class NoPage extends React.Component {
+    render() {
+
+        return <p>頁面維護中...</p>
+    }
+}
+
+export { About }
