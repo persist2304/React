5d62d0d8613f5487002d7159627069529d6bf310
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-07-30 12:18:06 +0800

    修掉上次的 bug ， Bug: 1) 星星 css 沒亮 2) 排版問題 3) 顯示日期的方式

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -30,24 +30,31 @@ class List extends React.Component{
     render(){
         return (
             <div className="listBlock">
+
                 <div className={" list " + (this.state.important == "Y" ? " important": "  " )}>
                     <input type="checkbox" className="taskChk" checked={this.state.complete}
                            onChange={this._changeState.bind(this,'complete')}/>
+
                     <input type="text" className={ "taskTitle" +
                             (this.state.important == "Y" ?" important": " ") +
                             (this.state.complete ? " complete": " ")}
                            value={this.props.listData.name}/>
+                    
                     <i className={this.state.important == "Y" ?
-                        " iconimportant far fa-star fa-lg icon":
+                        "fas fa-star fa-lg icon iconImportant":
                         "far fa-star fa-lg icon"}
                        onClick={this._changeState.bind(this,"important")}></i>
+
                     <i className="fas fa-pen fa-lg icon"></i>
+
                     <div className="listIcon">
                         {this.props.listData.date != ''?
                             <i className="fa fa-calendar-alt icon"></i>: " "}
-                        {this.props.listData.date != ""? ` ${this.props.listData.date.replace(/-/gi,'/')}` : ""}
+                        {this.props.listData.date != ""? ` ${this.props.listData.date.substring(5).replace(/-/gi,'/')}` : ""}
+
                         {this.props.listData.file != ""?
                             <i className="fas fa-file icon"></i>: " "}
+
                         {this.props.listData.commit != ""? <i className="far fa-comment-dots icon"></i>: " "}
 
                     </div>
