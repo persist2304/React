2296aa41fea659d068be1f29ca0b1ab2a7e64bf3
IvansSu <31758174+IvansSu@users.noreply.github.com>
2020-09-16 18:23:15 +0800

    install scss

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { ImageSource } from "../ImageSource"
 import { ImageForm } from "../ImageForm"
+import styles from "../../index.scss"
 
 class Home extends React.Component {
     render(){
         return (
             <div>
+                <span className={styles.hi}>123123</span>
                 {/*<ImageSource className="HomeBanner" src="https://picsum.photos/2000/300"/>*/}
                 <ImageSource className="HomeBanner" src=""/>
                 <ImageForm/>
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: ['./src/index.jsx'],
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './dist/'),
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./style/index.css",
+        }),
+    ],
     module: {
         rules: [
-            //第一個loader編譯JSX
-            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react'] } } },
-            //第二個loader編譯ES6
-            { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } } },
-            //編譯JSX的loader，將@babel/preset-env加進preset中
-            { test: /.jsx$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react','@babel/preset-env'] } } }
-        ]
+            {
+                test: /.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /.jsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: { localIdentName: '[name]__[local]___[hash:base64:5]' },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
     },
     //增加一個給 devserver 的設定
     devServer: {
